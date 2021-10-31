b0ef1ac08238869f9934fa0bb4820fb9564533a8
Matheusgomes432 <matheus.oliveirr@gmail.com>
2021-10-31 17:49:11 -0300

    inclusão aquivo java script

---
+++ script.js
@@ -0,0 +1,19 @@
+
+function Mudar_cor_menu() {
+    document.getElementById('menu').style.backgroundColor = 'blue';
+
+}
+function Mudar_cor_corpo() {
+    document.getElementById('corpo').style.backgroundColor = 'green';
+}
+function Mudar_imagem_menu() {
+    document.getElementById('menu').style.backgroundImage = "url(https://veja.abril.com.br/wp-content/uploads/2019/12/amazonia-floresta-coraccca7ao.jpg.jpg)";
+
+}
+function Mudar_imagem_corpo() {
+    document.getElementById('corpo').style.backgroundImage = "url(https://veja.abril.com.br/wp-content/uploads/2019/12/amazonia-floresta-coraccca7ao.jpg.jpg)";
+
+
+}
+
+
